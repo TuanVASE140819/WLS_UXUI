d1c4fa2640f3f95f7b905c4d30ea67bdf65795f5
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-05-08 17:30:18 +0700

    up code

--- src/components/relatedArticles/RelatedArticles.jsx
+++ src/components/relatedArticles/RelatedArticles.jsx
@@ -123,6 +123,11 @@ const RelatedArticles = () => {
                     <Link to="/post/1" className="relatedArticles__itemLeft">
                       <img
                         src={require("../../assets/images/post-1.png")}
+                        style={{
+                          width: "400px",
+                          height: "220px",
+                          objectFit: "cover",
+                        }}
                         alt=""
                       />
                       <p>
