6df4e27ecc5111b7f0fc3837ea0320c58b5d989b
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-05-14 13:46:10 +0700

    đăng ký

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -4,67 +4,188 @@ import SideBar from "../../components/sideBar/SideBar";
 import "./register.scss";
 import { Link } from "react-router-dom";
 import { Comments } from "../../components/comments/Comments";
-
+import { registerUser } from "../../api/apiServices";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { useNavigate } from "react-router-dom";
 const Register = () => {
+  const navigate = useNavigate();
+  const handleSumbit = (values, { setSubmitting }) => {
+    const data = {
+      username: values.username,
+      password: values.confirmPassword,
+      name: values.name,
+      role: "user",
+      dob: values.birthdate,
+      email: values.email,
+      note: values.note,
+      status: values.status,
+      obdata: values.obdata,
+    };
+
+    // neus Status Code = 200 => dang ky thanh cong => chuyen huong ve trang login
+    // neu Status Code = 400 => dang ky that bai => thong bao loi
+
+    registerUser(data)
+      .then((response) => {
+        console.log(response);
+        if (response.statusCode === 200) {
+          // in ra message do api tra ve
+          alert(response.message);
+          navigate("/login");
+        }
+        if (response.statusCode === 409) {
+          // in ra message do api tra ve
+          alert(response.message);
+        }
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <>
       <div className="register">
         <div className="register__wrapper">
-          <form className="register__form">
-            <div className="register__grid">
-              <div className="register__left">
-                <h2 className="register__title">Đăng ký</h2>
+          <Formik
+            initialValues={{
+              username: "",
+              gender: "",
+              birthdate: "",
+              email: "",
+              password: "",
+              confirmPassword: "",
+            }}
+            className="register__form"
+            validate={(values) => {
+              const errors = {};
+              // Add your validation logic here
+              if (!values.username) {
+                errors.username = "Bạn chưa nhập tên đăng nhập";
+              }
 
-                <input
-                  type="text"
-                  className="register__input"
-                  placeholder="Tên đăng nhập"
-                />
+              if (!values.email) {
+                errors.email = "Bạn chưa nhập email";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+              ) {
+                errors.email = "Bạn nhập email không đúng";
+              }
 
-                <div className="register__radio">
-                  {/* radio nam hay nữ */}
-                  <input type="radio" id="nam" name="gioitinh" value="nam" />
-                  <label htmlFor="nam" className="register__label">
-                    Nam
-                  </label>
-                  <input type="radio" id="nu" name="gioitinh" value="nu" />
-                  <label htmlFor="nu" className="register__label">
-                    Nữ
-                  </label>
+              if (!values.birthdate) {
+                errors.birthdate = "Bạn chưa nhập ngày sinh";
+              } else if (
+                new Date().getFullYear() -
+                  new Date(values.birthdate).getFullYear() <
+                18
+              ) {
+                errors.birthdate = "Bạn chưa đủ 18 tuổi";
+              }
+
+              if (!values.password) {
+                errors.password = "Bạn chưa nhập mật khẩu";
+              }
+
+              if (values.password !== values.confirmPassword) {
+                errors.confirmPassword = "Mật khẩu không khớp";
+              }
+
+              return errors;
+            }}
+            onSubmit={handleSumbit}
+          >
+            <Form className="register__form">
+              <div className="register__grid">
+                <div className="register__left">
+                  <h2 className="register__title">Đăng ký</h2>
+
+                  <Field
+                    type="text"
+                    name="username"
+                    className="register__input"
+                    placeholder="Tên đăng nhập"
+                  />
+                  <ErrorMessage
+                    name="username"
+                    component="div"
+                    className="error-message"
+                  />
+                  <div className="register__radio">
+                    {/* radio nam hay nữ */}
+                    <label>
+                      <Field type="radio" name="gender" value="nam" />
+                      Nam
+                    </label>
+                    <label>
+                      <Field type="radio" name="gender" value="nu" />
+                      Nữ
+                    </label>
+                  </div>
+                  <ErrorMessage
+                    name="gender"
+                    component="div"
+                    className="error-message"
+                  />
+                  <Field
+                    type="date"
+                    name="birthdate"
+                    className="register__input"
+                  />
+                  <ErrorMessage
+                    name="birthdate"
+                    component="div"
+                    className="error-message"
+                  />
+
+                  <Field
+                    type="text"
+                    name="email"
+                    className="register__input"
+                    placeholder="Email của bạn"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="error-message"
+                  />
+
+                  <Field
+                    type="password"
+                    name="password"
+                    className="register__input"
+                    placeholder="Mật khẩu"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="div"
+                    className="error-message"
+                  />
+
+                  <Field
+                    type="password"
+                    name="confirmPassword"
+                    className="register__input"
+                    placeholder="Nhập lại mật khẩu"
+                  />
+                  <ErrorMessage
+                    name="confirmPassword"
+                    component="div"
+                    className="error-message"
+                  />
+
+                  <button type="submit" className="register__butonRegister">
+                    Đăng ký
+                  </button>
                 </div>
-                <input
-                  type="date"
-                  placeholder="Ngày sinh"
-                  className="register__input"
-                />
-
-                <input
-                  type="text"
-                  placeholder="Email của bạn"
-                  className="register__input"
-                />
-                <input
-                  type="password"
-                  placeholder="Mật khẩu"
-                  className="register__input"
-                />
-                <input
-                  type="password"
-                  placeholder="Nhập lại mật khẩu"
-                  className="register__input"
-                />
-
-                <button className="register__butonRegister">Đăng ký</button>
+                <div className="register__right">
+                  <img
+                    src={require("../../assets/images/banner_login.png")}
+                    alt="weloveschool"
+                    width={350}
+                  />
+                </div>
               </div>
-              <div className="register__right">
-                <img
-                  src={require("../../assets/images/banner_login.png")}
-                  alt="weloveschool"
-                  width={350}
-                />
-              </div>
-            </div>
-          </form>
+            </Form>
+          </Formik>
         </div>
       </div>
     </>
