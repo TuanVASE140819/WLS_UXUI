adfac0f1894b680ef12d24f83f24315b426dcb3c
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-05-20 15:10:33 +0700

    upcode

--- src/pages/inforUser/InforUser.jsx
+++ src/pages/inforUser/InforUser.jsx
@@ -66,6 +66,53 @@ const InforUser = () => {
                 </span>
               </div>
             </div>
+            <div className="siderBarUser__ComboCode">
+              <div className="sideBarUser__code">
+                <div className="sideBarUser__codeItem">
+                  Mã giới thiệu: <span>123456</span>
+                </div>
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.3125 3.75C10.3125 3.25272 10.115 2.77581 9.76333 2.42417C9.41169 2.07254 8.93478 1.875 8.4375 1.875H3.75C3.25272 1.875 2.77581 2.07254 2.42417 2.42417C2.07254 2.77581 1.875 3.25272 1.875 3.75V8.4375C1.875 8.93478 2.07254 9.41169 2.42417 9.76333C2.77581 10.115 3.25272 10.3125 3.75 10.3125V6.5625C3.75 5.81658 4.04632 5.10121 4.57376 4.57376C5.10121 4.04632 5.81658 3.75 6.5625 3.75H10.3125Z"
+                    fill="#8F8F8F"
+                  />
+                  <path
+                    d="M11.25 4.6875C11.7473 4.6875 12.2242 4.88504 12.5758 5.23667C12.9275 5.58831 13.125 6.06522 13.125 6.5625V11.25C13.125 11.7473 12.9275 12.2242 12.5758 12.5758C12.2242 12.9275 11.7473 13.125 11.25 13.125H6.5625C6.06522 13.125 5.58831 12.9275 5.23667 12.5758C4.88504 12.2242 4.6875 11.7473 4.6875 11.25V6.5625C4.6875 6.06522 4.88504 5.58831 5.23667 5.23667C5.58831 4.88504 6.06522 4.6875 6.5625 4.6875H11.25Z"
+                    fill="#8F8F8F"
+                  />
+                </svg>
+              </div>
+
+              <div className="sideBarUser__code">
+                <div className="sideBarUser__codeItem">
+                  Link tài khoản: <span>httpt//hoahoweloves...</span>
+                </div>
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.3125 3.75C10.3125 3.25272 10.115 2.77581 9.76333 2.42417C9.41169 2.07254 8.93478 1.875 8.4375 1.875H3.75C3.25272 1.875 2.77581 2.07254 2.42417 2.42417C2.07254 2.77581 1.875 3.25272 1.875 3.75V8.4375C1.875 8.93478 2.07254 9.41169 2.42417 9.76333C2.77581 10.115 3.25272 10.3125 3.75 10.3125V6.5625C3.75 5.81658 4.04632 5.10121 4.57376 4.57376C5.10121 4.04632 5.81658 3.75 6.5625 3.75H10.3125Z"
+                    fill="#8F8F8F"
+                  />
+                  <path
+                    d="M11.25 4.6875C11.7473 4.6875 12.2242 4.88504 12.5758 5.23667C12.9275 5.58831 13.125 6.06522 13.125 6.5625V11.25C13.125 11.7473 12.9275 12.2242 12.5758 12.5758C12.2242 12.9275 11.7473 13.125 11.25 13.125H6.5625C6.06522 13.125 5.58831 12.9275 5.23667 12.5758C4.88504 12.2242 4.6875 11.7473 4.6875 11.25V6.5625C4.6875 6.06522 4.88504 5.58831 5.23667 5.23667C5.58831 4.88504 6.06522 4.6875 6.5625 4.6875H11.25Z"
+                    fill="#8F8F8F"
+                  />
+                </svg>
+              </div>
+              <button className="sideBarUser__recharge">Nạp</button>
+            </div>
+
             <div className="inforNav__links">
               <Link
                 to="/information?filter=posts"
@@ -138,56 +185,58 @@ const InforUser = () => {
         </div>
         <div className="information">
           <div className="information__wrapper">
-            <div className="information__listItem">
-              <figure className="information__image">
-                <img
-                  src={require("../../assets/images/sideBarMessage-1.png")}
-                  alt=""
-                />
-              </figure>
-              <div className="information__text">
-                <h4>Hòa Hồ</h4>
+            <div>
+              <div className="information__listItem">
+                <figure className="information__image">
+                  <img
+                    src={require("../../assets/images/sideBarMessage-1.png")}
+                    alt=""
+                  />
+                </figure>
+                <div className="information__text">
+                  <h4>Hòa Hồ</h4>
 
-                <span>
-                  {" "}
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M8.78751 1.21009C9.23551 0.133094 10.7635 0.133094 11.2115 1.21009L13.2935 6.21609L18.6975 6.65009C19.8615 6.74309 20.3335 8.19509 19.4465 8.95509L15.3295 12.4821L16.5865 17.7551C16.8575 18.8911 15.6225 19.7881 14.6265 19.1801L9.99951 16.3541L5.37251 19.1801C4.37651 19.7881 3.14151 18.8901 3.41251 17.7551L4.66951 12.4821L0.552514 8.95509C-0.334486 8.19509 0.137514 6.74309 1.30151 6.65009L6.70551 6.21609L8.78751 1.21009Z"
-                      fill="#FCAA17"
-                    />
-                  </svg>
-                  9015
-                </span>
+                  <span>
+                    {" "}
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M8.78751 1.21009C9.23551 0.133094 10.7635 0.133094 11.2115 1.21009L13.2935 6.21609L18.6975 6.65009C19.8615 6.74309 20.3335 8.19509 19.4465 8.95509L15.3295 12.4821L16.5865 17.7551C16.8575 18.8911 15.6225 19.7881 14.6265 19.1801L9.99951 16.3541L5.37251 19.1801C4.37651 19.7881 3.14151 18.8901 3.41251 17.7551L4.66951 12.4821L0.552514 8.95509C-0.334486 8.19509 0.137514 6.74309 1.30151 6.65009L6.70551 6.21609L8.78751 1.21009Z"
+                        fill="#FCAA17"
+                      />
+                    </svg>
+                    9015
+                  </span>
+                </div>
+                <div className="information__logout" onClick={handleSignOut}>
+                  <p>Đăng xuất</p>
+                </div>
+                <div className="information__lang">
+                  <NavLink to="/?lang=vi" className="information__langLink">
+                    VI
+                  </NavLink>
+                  <span>|</span>
+                  <NavLink to="/?lang=en" className="information__langLink">
+                    EN
+                  </NavLink>
+                </div>
               </div>
-              <div className="information__logout" onClick={handleSignOut}>
-                <p>Đăng xuất</p>
+              <div className="information__links">
+                <Link
+                  to="/information?filter=posts"
+                  className={`information__linkItem ${
+                    selectedInterface === "posts" ? "active" : ""
+                  }`}
+                  onClick={() => setSelectedInterface("posts")}
+                ></Link>
               </div>
-              <div className="information__lang">
-                <NavLink to="/?lang=vi" className="information__langLink">
-                  VI
-                </NavLink>
-                <span>|</span>
-                <NavLink to="/?lang=en" className="information__langLink">
-                  EN
-                </NavLink>
-              </div>
-            </div>
-            <div className="information__links">
-              <Link
-                to="/information?filter=posts"
-                className={`information__linkItem ${
-                  selectedInterface === "posts" ? "active" : ""
-                }`}
-                onClick={() => setSelectedInterface("posts")}
-              ></Link>
             </div>
           </div>
           <div className="information__links">
