7f6d0f876a5f07b477d65c8a1870cda3e5009c57
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-06-13 10:15:33 +0700

    upcode

--- src/pages/miniGame/miniGame.jsx
+++ src/pages/miniGame/miniGame.jsx
@@ -8,7 +8,7 @@ const Minigame = () => {
       <div className="MinigamePage">
         <div className="SidbarGame">
           <div className="SidbarGame__wrapper">
-            <Link
+            {/* <Link
               to="/quiz"
               className="SidbarGame__item"
               style={{ textDecoration: "none" }}
@@ -56,50 +56,38 @@ const Minigame = () => {
                 />
               </svg>
               Luật chơi
-            </Link>
+            </Link> */}
           </div>
         </div>
         <section className="Minigame">
-          <img src={require("../../assets/images/quiz.png")} alt="minigame" />
-          <h3 className="Minigame__title">Làm bài kiểm tra nhận ngay điểm</h3>
-          <div className="Minigame__rule">
-            <div className="Minigame__time">
-              <div className="Minigame__time--content">
-                <div className="Minigame__title--content">Thời gian </div>
-              </div>
-              <div className="Minigame__time--content">
-                <h4>10 phút</h4>
-              </div>
+          <div className="Minigame__wrapper">
+            <div className="Minigame__item">
+              <img
+                src={require("../../assets/images/minigame-01.png")}
+                alt=""
+              />
+              <h4>Vòng quay may mắn</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer
+                <br />
+                adipiscing elit, sed diam nonummy{" "}
+              </p>
             </div>
-            <div className="Minigame__point">
-              <div className="Minigame__title--content">Lượt chơi</div>
-              <div className="Minigame__point--content">2 Lượt</div>
+            <div className="Minigame__item">
+              <img
+                src={require("../../assets/images/minigame-02.png")}
+                alt=""
+              />
+              <h4>Vòng quay may mắn</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer
+                <br />
+                adipiscing elit, sed diam nonummy{" "}
+              </p>
             </div>
-          </div>
-          <div className="Minigame__btn">
-            <button className="Minigame__btn--choose">Kiểm tra 15 phút</button>
-            <button className="Minigame__btn--choose">1 tiết</button>
-            <button className="Minigame__btn--choose">Học kì</button>
           </div>
         </section>
-        <div className="SidbarRule">
-          <div className="SidbarRule__wrapper">
-            <Link
-              to="/quiz"
-              className="SidbarGame__item"
-              style={{ textDecoration: "none" }}
-            >
-              Chính sách điểm thưởng
-            </Link>
-            <Link
-              to="/quiz"
-              className="SidbarGame__item"
-              style={{ textDecoration: "none" }}
-            >
-              Chính sách đổi quà
-            </Link>
-          </div>
-        </div>
+        <div className="SidbarRule"></div>
       </div>
     </>
   );
